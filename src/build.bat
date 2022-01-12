dabfa6229ac876f79b3f76e290a1ec2a8d1a4b7e
Luis J Pacheco <89895893+fredabomb50@users.noreply.github.com>
2022-01-12 08:06:21 -0500

    Delete build.bat

--- src/build.bat
+++
@@ -1,3 +0,0 @@
-@echo OFF
-cd C:\Users\luis.pacheco\Documents\Tools\_Ahk\Compiler
-Ahk2Exe.exe /in "C:\Users\luis.pacheco\Documents\AHK Projects\ProjectBuilder\src\main.ahk" /out "C:\Users\luis.pacheco\Documents\AHK Projects\ProjectBuilder\build\ProjectBuilder.exe"
